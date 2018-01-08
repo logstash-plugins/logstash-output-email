a777e19ee9bf77045ff43b1b6ccbaeccdd423693
Julien Bordellier <1444415+jstoja@users.noreply.github.com>
2018-01-08 14:20:21 +0100

    Add mustache templating for email body (#51)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,17 @@ class LogStash::Filters::NOOP < LogStash::Filters::Base
     filter_matched(event)
   end
 end
+
+def craft_multi_part_email(body, htmlbody, content_type)
+  charset = "charset=UTF-8"
+  content_encoding = "Content-Transfer-Encoding: 7bit"
+ 
+  # Have to get the generated part of the header, eg:
+  # --==_mimepart_5a462a1fda04b_efe7d032127
+  mimetype = content_type.split('; ')[1][10..-2]
+
+  plain_header = "\n--#{mimetype}\nContent-Type: text/plain;\n #{charset}\n#{content_encoding}\n\n"
+  html_header = "\n--#{mimetype}\nContent-Type: text/html;\n #{charset}\n#{content_encoding}\n\n"
+
+  "#{plain_header}#{body}#{html_header}#{htmlbody}\n\n--#{mimetype}--\n"
+end
